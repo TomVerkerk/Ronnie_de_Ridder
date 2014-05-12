55ad94738246ab17884573bda3ff52aa497f5aa0
Nolimet <nolimet@stamdata.nl>
2014-05-12 16:05:48 +0200

    something :D

---
+++ Assets/Scripts/Debug/DebugRot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugRot : MonoBehaviour {
+
+    public Transform toTrack;
+	void Update () 
+    {
+        guiText.text = ("Rot: " + toTrack.rotation + "\nPos: " + toTrack.position);
+	}
+}
