b95ad6538ce39873349c48d7b1488459ec8b2b81
Nolimet <nolimet@stamdata.nl>
2014-05-13 10:02:03 +0200

    Added simple base class

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : BaseTrackingHandler, ITrackableEventHandler
+{
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        Renderer[] renderss = GetComponentsInChildren<Renderer>(true);
+
+        foreach (Renderer r in renderss)
+        {
+            r.material.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
