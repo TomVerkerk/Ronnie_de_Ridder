cf3d17acb908a24f5c464c26990a1fb7e235b77c
Nolimet <nolimet@stamdata.nl>
2014-05-16 13:46:59 +0200

    updated ignor

--- Assets/Scripts/Tests/MoveObject.cs
+++ Assets/Scripts/Tests/MoveObject.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class MoveObject : BaseTrackingHandler, ITrackableEventHandler
 {
+    public delegate void ObjectUpdater();
 
+    public ObjectUpdater updater;
     protected override void OnTrackingFound()
     {
+        if (updater != null)
+            updater();
+
         base.OnTrackingFound();
         Renderer[] renderss = GetComponentsInChildren<Renderer>(true);
 
@@ -13,5 +18,13 @@ public class MoveObject : BaseTrackingHandler, ITrackableEventHandler
         {
             r.material.color = new Color(Random.value, Random.value, Random.value);
         }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        if(updater!=null)
+            updater();
+
+        base.OnTrackingLost();
     }
 }
---
+++ Assets/Scripts/Tests/Object.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+    bool rooted = false;
+    Quaternion orginalRot;
+	// Use this for initialization
+	void Start () {
+        transform.parent.gameObject.GetComponent<MoveObject>().updater+=rotate;
+
+        orginalRot = transform.rotation;
+	}
+
+    void rotate()
+    {
+        rooted = !rooted;
+
+        if (rooted)
+        {
+            transform.Rotate(20, 40, 0);
+        }
+        else
+        {
+            transform.rotation = orginalRot;
+        }
+    }
+
+}
