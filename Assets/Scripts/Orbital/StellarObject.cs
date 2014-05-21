8d71dc37471cdd232d6f0c82f5f1bcf8fb9251ff
Nolimet <nolimet@stamdata.nl>
2014-05-21 09:44:47 +0200

    added blackhole

---
+++ Assets/Scripts/Orbital/StellarObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Orbital
+{
+    public class StellarObject : MonoBehaviour
+    {
+
+        public float mass = 0;
+
+        public bool active = false;
+    }
+}
