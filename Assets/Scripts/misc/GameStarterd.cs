b070dc314208887f3232e7d0cfa729ed47c43cdb
Nolimet <nolimet@stamdata.nl>
2014-05-26 16:44:04 +0200

    added a load bar and a exit button

---
+++ Assets/Scripts/misc/GameStarterd.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Loader))]
+public class GameStarterd : MonoBehaviour {
+
+    public string load;
+
+    void Start()
+    {
+        GetComponent<Loader>().SyncLoadLevel(load);
+    }
+}
---
+++ Assets/Scripts/misc/Loader.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Loader : MonoBehaviour
+{
+    #region Vars
+    public Texture2D emptyProgressBar; // Set this in inspector.
+    public Texture2D fullProgressBar; // Set this in inspector.
+
+    private AsyncOperation async = null; // When assigned, load is in progress.
+
+    private Vector2 screenCenter;
+    #endregion
+
+    void Start()
+    {
+        screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+    }
+
+    public void SyncLoadLevel(string levelName)
+    {
+        async = Application.LoadLevelAsync(levelName);
+        Load();
+    }
+
+    IEnumerator Load()
+    {
+        yield return async;
+    }
+
+    void OnGUI()
+    {
+        if (async != null)
+        {
+            GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100, 50), emptyProgressBar);
+            GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100 * async.progress, 50), fullProgressBar);
+            GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+            GUI.Label(new Rect(screenCenter.x, screenCenter.y+50, 100, 50), string.Format("{0:N0}%", async.progress * 100f));
+        }
+    }
+}
