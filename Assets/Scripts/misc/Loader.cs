8ee9ba965ad6ed3b43c1f30ab4b0f5231a45df74
Nolimet <nolimet@stamdata.nl>
2014-05-27 09:29:53 +0200

    sorted a few thing

--- Assets/Scripts/misc/Loader.cs
+++ Assets/Scripts/misc/Loader.cs
@@ -11,11 +11,13 @@ public class Loader : MonoBehaviour
     private AsyncOperation async = null; // When assigned, load is in progress.
 
     private Vector2 screenCenter;
+    private Vector2 screenSize;
     #endregion
 
     void Start()
     {
         screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        screenSize = new Vector2(Screen.width, Screen.height);
     }
 
     public void SyncLoadLevel(string levelName)
@@ -33,10 +35,16 @@ public class Loader : MonoBehaviour
     {
         if (async != null)
         {
-            GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100, 50), emptyProgressBar);
-            GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100 * async.progress, 50), fullProgressBar);
+            //GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100, 50), emptyProgressBar);
+           // GUI.DrawTexture(new Rect(screenCenter.x, screenCenter.y, 100 * async.progress, 50), fullProgressBar);
+            
+            GUI.DrawTexture(new Rect(screenCenter.x, screenSize.y - 100, 100, 50), emptyProgressBar);
+            GUI.DrawTexture(new Rect(screenCenter.x - screenSize.x * 100 - 6f, screenSize.y - 100, screenSize.x - 6f * 100, 50), fullProgressBar);
+            //async.progress
+            
             GUI.skin.label.alignment = TextAnchor.MiddleCenter;
             GUI.Label(new Rect(screenCenter.x, screenCenter.y+50, 100, 50), string.Format("{0:N0}%", async.progress * 100f));
+
         }
     }
 }
