ec2e5639ba283e041faad81f7f783e6c5bbf4df0
Nolimet <nolimet@stamdata.nl>
2014-05-28 12:02:30 +0200

    stuff

---
+++ Assets/Scripts/UI/BlinkingText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour {
+
+    Material m;
+	void Start () {
+        m = GetComponent<TextMesh>().renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m.color = new Color(1,1,1,Mathf.PingPong(Time.time / 3f, 0.8f)+0.2f);
+	}
+}
