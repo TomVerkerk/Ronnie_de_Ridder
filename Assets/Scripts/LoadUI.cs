5e4f871583dd12d4c2f41ae5b36472597324ab04
Nolimet <nolimet@stamdata.nl>
2014-06-16 13:33:25 +0200

    added load script for UI

---
+++ Assets/Scripts/LoadUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevelAdditive("UI-Cam");
+    }
+}
